6f2f4ee4060b8d660bcd2df3b4653ac442e780f7
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-10 18:29:45 +0600

    service section

--- src/pages/Home/Services.jsx
+++ src/pages/Home/Services.jsx
@@ -1,8 +1,12 @@
 
 const Services = () => {
     return (
-        <div>
-            Services
+        <div className="my-24">
+            <div className="text-center space-y-3">
+                <h1 className="text-primary font-semibold text-lg">Service</h1>
+                <h2 className="text-4xl font-bold ">Our Service Area</h2>
+                <p className="">the majority have suffered alteration in some form, by injected humour, or randomized <br /> words which dont look even slightly believable. </p>
+            </div>
         </div>
     );
 };
