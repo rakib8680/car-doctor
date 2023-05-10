2bc5b17c5a42136ee33e3783d30aba79bb7535ca
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-10 19:23:47 +0600

    react icons

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,15 @@
 
+import login_img from '../../assets/images/login/login.svg'
+
+
+
 const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center lg:text-left w-1/2">
+                    {/* <h1 className="text-5xl font-bold">Login now!</h1> */}
+                    <img src={login_img} />
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -26,6 +31,10 @@ const Login = () => {
                         </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
+                        </div>
+                        <div className='text-center text-sm'>
+                            <p>or</p>
+                            <h2>Login With</h2>
                         </div>
                     </div>
                 </div>
