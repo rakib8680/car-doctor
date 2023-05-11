102b82098334edc78449a115c42ce9235dd991d2
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-11 21:25:52 +0600

    checkout form

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,8 @@ const router = createBrowserRouter([
         path: 'checkout/:id',
         element: <CheckOut></CheckOut>,
         loader: ({params})=>fetch(`http://localhost:5000/services/${params.id}`)
-      }
+      },
+      
     ]
   }
 ])
--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,14 +1,69 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../providers/AuthProvider';
 
 const CheckOut = () => {
 
     const data = useLoaderData()
-    console.log(data);
+    const { user } = useContext(AuthContext)
+
+    const handleBookService = event => {
+        event.preventDefault();
+        const form = event.target
+
+        const email = user.email;
+        const name = form.name.value
+        const date = form.date.value
+
+        const order = {
+            customerName: name,
+            email,
+            date,
+            price : data?.price,
+            service: data?._id
+        }
+
+
+    }
 
     return (
-        <div>
-            Book
+
+        <div className='flex flex-col items-center'>
+            <div>
+                <h2 className='text-3xl font-semibold mt-10'>Book Service : <span className='text-primary'>{data?.title}</span></h2>
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-md border border-base-300 bg-base-100 px-10 py-7 my-20">
+
+                <form className='' onSubmit={handleBookService}>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"> Name</span>
+                        </label>
+                        <input type="text" defaultValue={user?.displayName} name='name' className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Amount</span>
+                        </label>
+                        <input type="text" defaultValue={'$' + data?.price} name='phone' className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" defaultValue={user?.email} name='email' className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <input type="date"  name='date' className="input input-bordered" />
+                    </div>
+                    <div className="form-control mt-6">
+                        <input type="submit" value="Confirm Order" className='btn btn-primary btn-block' />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
